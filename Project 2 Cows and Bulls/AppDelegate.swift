060224fffe61395be93a1de3953393d45939cff0
Marc Aupont <marc@digimarktech.com>
2017-06-07 17:37:49 -0400

    Initial Commit

---
+++ Project 2 Cows and Bulls/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project 2 Cows and Bulls
+//
+//  Created by Marc Aupont on 6/7/17.
+//  Copyright © 2017 Digimark Technical Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project 2 Cows and Bulls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project 2 Cows and Bulls
+//
+//  Created by Marc Aupont on 6/7/17.
+//  Copyright © 2017 Digimark Technical Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
